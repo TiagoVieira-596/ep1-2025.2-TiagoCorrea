42464379c4c3ac49f3de9f3b2303b0e83a848732
Tiago <tvhollowknight@gmail.com>
2025-09-28 19:15:37 -0300

    feat: criei uma classe Verificador para checar se certos inputs são Válidos

---
+++ sistema_hospital/src/main/java/br/hospital/utils/Verificador.java
@@ -0,0 +1,24 @@
+package br.hospital.utils;
+
+public class Verificador {
+  public static boolean cpfValido(String cpf) {
+    String numerosCpf = cpf.replace(".", "").replace("-", "");
+    if (!numerosCpf.matches("\\d{11}") || numerosCpf.matches("(\\d)\\1{10}")) {
+      return false;
+    } else {
+      for (int i = 0; i < 2; i++) {
+        int soma = 0;
+        int peso = 10 + i;
+        for (int j = 0; j < 9 + i; j++) {
+          soma += Character.getNumericValue(numerosCpf.charAt(j)) * (peso - j);
+        }
+        int resto = (10 * soma) % 11;
+        if (resto == 10) resto = 0;
+        if (resto != Character.getNumericValue(numerosCpf.charAt(9 + i))) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
