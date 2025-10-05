f40798947ccc7c493f52547930973799f0b703cc
Tiago <tvhollowknight@gmail.com>
2025-10-05 01:37:56 -0300

    feat: Finalizei a classe usada para a serialização dos arquivos.

--- sistema_hospital/src/main/java/br/hospital/utils/Serializador.java
+++ sistema_hospital/src/main/java/br/hospital/utils/Serializador.java
@@ -2,33 +2,56 @@ package br.hospital.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
+
+import br.hospital.menu.Tela;
 
 public class Serializador {
-  public static void serializar(Object objeto, String arquivo, Screen tela, TextGraphics tg) throws IOException{
+  public static <T> void serializar(T objeto, String arquivo, Class<T[]> tipoArray) throws IOException{
     Gson gson = new Gson();
-    String json = gson.toJson(objeto);
+    Path caminho = Path.of("data/", arquivo);
+
+    List<T> listaDeDados = new ArrayList<>();
+
     try {
-      Files.createDirectories(Path.of("data"));
-      Files.writeString(Path.of("data/" + arquivo), json);
+      if (Files.exists(caminho)) {
+        String dadosNoArquivo = Files.readString(caminho);
+        T[] arrayExistente = gson.fromJson(dadosNoArquivo, tipoArray);
+        if (arrayExistente != null) {
+            listaDeDados.addAll(Arrays.asList(arrayExistente));
+        }
+      }
     } catch (IOException e) {
-      tg.putString(1, 0, "Não foi possível salvar o arquivo.");
-      tela.refresh();
+        Tela.exibirMensagem("Não foi possível ler o arquivo");
+    } catch (JsonSyntaxException e) {
+        Tela.exibirMensagem("JSON inválido no arquivo: " + arquivo);
     }
+
+    listaDeDados.add(objeto);
+
+    String json = gson.toJson(listaDeDados);
+    Files.writeString(caminho, json);
   }
-  public static <T> T deserializar(Class<T> classe, String arquivo, Screen tela, TextGraphics tg) throws IOException{
+
+  public static <T> T[] deserializar(String arquivo, Class<T[]> tipoArray) throws IOException {
     Gson gson = new Gson();
+    Path caminho = Path.of("data", arquivo);
+
     try {
-      String dados = Files.readString(Path.of("data/" + arquivo));
-      return gson.fromJson(dados, classe);
-    } catch (IOException | JsonSyntaxException e) {
-      tg.putString(1, 0, "Não foi possível lêr o arquivo.");
-      tela.refresh();
+        String conteudo = Files.readString(caminho);
+        return gson.fromJson(conteudo, tipoArray);
+    } catch (IOException e) {
+        Tela.exibirMensagem(1, 0, "Não foi possível ler o arquivo");
+    } catch (JsonSyntaxException e) {
+        Tela.exibirMensagem(1, 0, "JSON inválido no arquivo: " + arquivo);
     }
+
     return null;
   }
+
 }
